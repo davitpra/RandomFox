4c463d55dd31053b5e16aad7e8537745af2a73de
David <j.david.prado1716@gmail.com>
2023-05-09 16:09:59 -0500

    State con otros tipos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
 import Head from 'next/head'
 import { NextPage } from 'next';
 import { RandomFox } from '@/components/RandomFox'
+import { useState } from "react";
 
+// generate simple unique id
+const generateId = (): string => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+};
+// random number from 1 to 122
 const randomNumber = () => Math.floor(Math.random() * 122) + 1;
 
+type ImageItem ={id:string, url:string}
+
 const Home: NextPage = () => {
-  const image = `https://randomfox.ca/images/${randomNumber()}.jpg`;
+
+  const [images, setImages] = useState<Array<ImageItem>>([
+    {id:generateId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg` },
+    {id:generateId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg` },
+    {id:generateId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg` },
+    {id:generateId(), url:`https://randomfox.ca/images/${randomNumber()}.jpg` },
+  ]);
+
   return (
+
     <>
       <Head>
         <title>React with TypeScript</title>
@@ -15,8 +34,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <h1 className='text-xl text-blue-500'> Hola Davidcito</h1>
-        <RandomFox image ={image} alt= {`imagen de zorro ${randomNumber()}`}/>
+        {images.map(({id, url}) => (
+          <div key={id} className="p-4">
+            <RandomFox image={url} alt={`fox`}/>
+          </div>
+        ))}
       </main>
     </>
   )
